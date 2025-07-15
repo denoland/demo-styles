950bd58ec6f341ae096931194b68c05b977ae82c
Phil Hawksworth <phil@deno.com>
2025-07-15 14:45:40 +0200

    handle cors

--- _config.ts
+++ _config.ts
@@ -2,9 +2,19 @@ import lume from "lume/mod.ts";
 import sass from "lume/plugins/sass.ts";
 
 
+async function cors(request: Request, next: Lume.RequestHandler) {
+  const response = await next(request);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
+}
+
+
 const site = lume({
   src: "./src",
   dest: "./_site",
+  server: {
+    middlewares: [cors]
+  }
 });
 
 
@@ -13,6 +23,9 @@ site.add("styles.scss");
 
 // Pass through the static assets
 site.add("static", ".");
+
+
+
 
 
 export default site;
