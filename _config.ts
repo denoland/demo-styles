c3e42e62a290f8a4be55bd52adbdd282b0a664a9
Phil Hawksworth <phil@deno.com>
2025-07-17 11:01:14 +0200

    serve dynamically for CORS headers

--- _config.ts
+++ _config.ts
@@ -1,32 +1,17 @@
 import lume from "lume/mod.ts";
 import sass from "lume/plugins/sass.ts";
 
-
-async function cors(request: Request, next: Lume.RequestHandler) {
-  const response = await next(request);
-  console.log("allowing cors for", request.url);
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  return response;
-}
-
-
 const site = lume({
   src: "./src",
   dest: "./_site",
-  server: {
-    middlewares: [cors]
-  }
 });
 
-
+// Sass
 site.use(sass(/* Options */));
 site.add("styles.scss");
 
 // Pass through the static assets
 site.add("static", ".");
-
-
-
 
 
 export default site;
--- serve.ts
+++ serve.ts
@@ -1,10 +1,15 @@
 import Server from "lume/core/server.ts";
 
-const server = new Server({
-  port: 8000,
-  root: `${Deno.cwd()}/_site`,
-});
+const corsMiddleware = async (request: Request, next: Lume.RequestHandler) => {
+  const response = await next(request);
+  console.log("allowing cors for", request.url);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
+}
 
+export const server = new Server({ root: "./_site" });
+
+server.use(corsMiddleware);
 server.start();
 
 console.log("Listening on http://localhost:8000");
