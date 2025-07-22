387c413126814ec0fdac1f54053a11ce7fe019de
Phil Hawksworth <phil@deno.com>
2025-07-22 11:50:20 +0100

    log referring hostname

--- serve.ts
+++ serve.ts
@@ -2,6 +2,15 @@ import Server from "lume/core/server.ts";
 
 const corsMiddleware = async (request: Request, next: Lume.RequestHandler) => {
   const response = await next(request);
+  const referrer = request.referrer;
+
+  // Check if there is a referrer
+  if (referrer) {
+      console.log("Referring domain:", new URL(referrer).hostname);
+  } else {
+      console.log("No referrer available.");
+  }
+
   console.log("allowing cors for", request.url);
   response.headers.set("Access-Control-Allow-Origin", "*");
   return response;
